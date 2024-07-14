b5f241150473306d8e23559a5d54250d6bb7a4c0
Fedy1507 <gayratovfirdavs720@gmail.com>
2024-07-14 21:12:45 +0500

    first commit

---
+++ pkg/db.go
@@ -0,0 +1,28 @@
+package pkg
+
+import (
+	config "item_service/config"
+	_ "github.com/lib/pq"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	config := config.Load()
+	conn := fmt.Sprintf(`host = %s port = %d user = %s dbname = %s password = %s sslmode = disable`,
+		config.DB_HOST, config.DB_PORT, config.DB_USER, config.DB_NAME, config.DB_PASSWORD)
+
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
